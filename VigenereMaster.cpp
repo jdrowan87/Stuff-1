77b63ad3a08ee5ab4cb5ac6d5f319c881985bccf
joshua rowan <47506412+jdrowan87@users.noreply.github.com>
2020-04-23 07:42:29 -0500

    a
    a

---
+++ VigenereMaster.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void decryptVigenere(string input,int startSize, int endSize,char letter);
+const int START = 3;
+const int END = 10;
+int main(){
+	
+	char charFreq1;
+	cout<<"Welcome to Vigenere Cipher Multiple Language Decryptor!\n";
+	char x;
+	for (int i = 0; i<300; i++){
+	x=i;
+	cout<<x;
+	}
+	
+}
+
+void decrypt(string input){
+	
+} 
